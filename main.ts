be977ffc27029b6cb8bb4e298e2868ca33eef3b2
Matthew Gromer <matt.gromer@gmail.com>
2025-06-04 10:00:14 -0500

    Document tidy command

--- main.ts
+++ main.ts
@@ -11,6 +11,36 @@ const DEFAULT_SETTINGS: TidyTasksSettings = {
 export default class TidyTasksPlugin extends Plugin {
     settings: TidyTasksSettings;
 
+    async tidyCompletedTasks() {
+        if (!this.settings.removeCompleted) {
+            new Notice('Tidy Tasks is disabled in settings');
+            return;
+        }
+
+        const files = this.app.vault.getMarkdownFiles();
+        let removed = 0;
+
+        for (const file of files) {
+            const content = await this.app.vault.read(file);
+            const lines = content.split(/\r?\n/);
+            const newLines: string[] = [];
+
+            for (const line of lines) {
+                if (/^- \[[xX]\] .*(🔁|every)/.test(line)) {
+                    removed++;
+                    continue;
+                }
+                newLines.push(line);
+            }
+
+            if (newLines.length !== lines.length) {
+                await this.app.vault.modify(file, newLines.join('\n'));
+            }
+        }
+
+        new Notice(`Removed ${removed} completed repeating task${removed === 1 ? '' : 's'}.`);
+    }
+
     async onload() {
         await this.loadSettings();
 
@@ -22,9 +52,7 @@ export default class TidyTasksPlugin extends Plugin {
         this.addCommand({
             id: 'tidy-completed-tasks',
             name: 'Tidy Completed Tasks',
-            callback: () => {
-                new Notice('Would tidy completed tasks');
-            }
+            callback: () => this.tidyCompletedTasks()
         });
 
         this.addSettingTab(new TidyTasksSettingTab(this.app, this));
